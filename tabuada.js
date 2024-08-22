c14f33368a2782d71324db98fda3bfd0ae70a039
Felipe Augusto <felipeaugusto030412@gmail.com>
2024-08-22 20:01:56 -0300

    adição da tabuada completa

---
+++ tabuada.js
@@ -0,0 +1,7 @@
+// Número para o qual queremos gerar a tabuada
+let numero = 5;
+
+// Loop para iterar de 1 até 10
+for (let i = 1; i <= 10; i++) {
+  console.log(`${numero} x ${i} = ${numero * i}`);
+}
